21ef29df395a88f762476d07eceebab083f8a6b9
HARSHIT YADAV <govindchandrayadav7379@gmail.com>
2023-07-31 10:16:07 +0530

    commit

---
+++ Java Program Set 3/copyString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class copyString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String strCopy = str;
+        System.out.println(strCopy);
+    }
+}
