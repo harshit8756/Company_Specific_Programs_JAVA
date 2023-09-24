7449aabd123dbbcc4b99b09c5796ba09c571321f
HARSHIT YADAV <govindchandrayadav7379@gmail.com>
2023-09-24 10:53:44 +0530

    main

--- Java Program Set 4/removeDuplicateNumberInArray.java
+++ Java Program Set 4/removeDuplicateNumberInArray.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
 public class removeDuplicateNumberInArray {
     public static void main(String[] args) {
-
+        int[] inputArray = {1, 2, 3, 4, 3, 2, 1, 6, 7, 8};
+        removeDuplicate(inputArray);
+    }
+    public static  void removeDuplicate(int[] inputArray)
+    {
+        LinkedHashSet<Integer> arr1 = new LinkedHashSet<>();
+        for (int i = 0; i < inputArray.length; i++)
+        {
+            arr1.add(inputArray[i]);
+        }
+        System.out.println(arr1);
     }
 }
