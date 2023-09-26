5fd80d30ac4c59c03f4bd8cb6cadb84a4fc9de89
HARSHIT YADAV <govindchandrayadav7379@gmail.com>
2023-09-27 00:52:24 +0530

    main

--- Java Program Set 4/nonRepeatingElementInArray.java
+++ Java Program Set 4/nonRepeatingElementInArray.java
@@ -1,17 +1,5 @@
 public class nonRepeatingElementInArray {
     public static void main(String[] args) {
-        int[] arr = {1,1,2,2,3,4,5,3,5};
-        for (int i = 0; i < arr.length; i++)
-        {
-            for (int j = i+1; j <arr.length; j++)
-            {
-             if ( i!=j && arr[i] == arr[j])
-             {
-                 System.out.println(arr[j]);
-             }
-             break;
 
-            }
-        }
     }
 }
